0b62ff7522b075c01ff06698b23769112f23c99f
anastasristeski <anastas.risteski5@gmail.com>
2025-08-18 15:09:37 +0200

    baseurlchanged

--- frontend-app/src/api/axiosClient.jsx
+++ frontend-app/src/api/axiosClient.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  //baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://e-commerce-backend-base-production.up.railway.app/api',
   headers: {  
     'Accept': 'application/json',
   },
