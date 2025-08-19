0ed4b3d211ee307c48ae5841da992429c3ea2b0c
anastasristeski <anastas.risteski5@gmail.com>
2025-08-19 11:03:08 +0200

    ss

--- frontend-app/src/main.jsx
+++ frontend-app/src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../src/styles/main.scss'
 import App from './App.jsx'
-
+if('ontouchstart' in window || navigator.maxTouchPoints >0){
+  document.body.classList.add('no-hover');
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
